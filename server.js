153bc71f151bc478389888f782bd6c6c562939d4
Jessica Peters <fullerenedream@gmail.com>
2022-06-07 00:11:25 -0600

    Set up and serve simple static assets

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+const path = require('path');
 const express = require('express');  
 const app = express();
 
-app.get('/', (req, res) => {  
-    res.send({ message: 'Hello World!' });  
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/public/index.html`);
 });
 
 app.listen(3141, () => {  
