b0a954b442c51855f997e147fc3b84150359ce96
Jessica Peters <fullerenedream@gmail.com>
2022-06-07 23:15:09 -0600

    Display log of latlongs for current user
    Get latlong from each click event and display log of latlongs for the current user. Latlongs not yet logged persistently for all users

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const app = express()
 
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs')
+app.use(express.json());
+// app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));
+// app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
   res.render('index');
